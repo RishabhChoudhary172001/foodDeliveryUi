40ceac556d2dd4f294ec82f293b737ff3c43fa21
RishabhChoudhary172001 <rishabhchoudhary172001@gmail.com>
2024-05-19 11:14:50 +0530

    navbar responsive complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,9 +12,9 @@ const links = [
 const Navbar = () => {
   const user = false;
   return (
-    <div className="h-12 md:h-24 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+    <div className="h-12 md:h-24 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase lg:px-20 xl:px-40">
       {/* {LEFT LINKS} */}
-      <div className="hidden md:flex gap-4 flex-1">
+      <div className="hidden md:flex gap-2 md:gap-10 flex-1">
         {links.map((item) => (
           <Link key={item.id} href={item.url}>
             {item.title}
@@ -31,8 +31,8 @@ const Navbar = () => {
         <Menu />
       </div>
       {/* {RIGHT LINKS} */}
-      <div className="hidden md:flex gap-4 items-center justify-center flex-1">
-        <div className="flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
           <Image src="/phone.png" alt="" width={20} height={20}/>
           <span>123 456 78</span>
         </div>
